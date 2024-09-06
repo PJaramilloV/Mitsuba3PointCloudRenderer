fbcd7d1d7e93ee10e6316226e972bf19d47ad431
Cubolink <70733191+Cubolink@users.noreply.github.com>
2024-09-06 19:43:30 -0400

    feat: 2_pc.py allows .obj. Adds configurable up.
    - The 2_pc.py can handle now .obj files.
    - The up-vector direction is configurable now in the
    render_mitsuba3_2pc.py too (it was configurable in the
    render_mitsuba3.py since some commits ago)

--- render_mitsuba3_2pc.py
+++ render_mitsuba3_2pc.py
@@ -21,6 +21,27 @@ def read_ply(path):
     return pcl_time, pcl_time_rgb
 
 
+def read_obj(path):
+    import pywavefront
+    
+    evaluated_vertices = pywavefront.Wavefront(path)
+    evaluated_vertices = np.array(evaluated_vertices.vertices)
+    
+    pcl_time_rgb = evaluated_vertices[:, 3:]
+    pcl_time = evaluated_vertices[:, :3]
+    
+    if len(np.shape(pcl_time)) < 3:
+        new_size = (1,) + np.shape(pcl_time)
+        pcl_time.resize(new_size)
+        if pcl_time_rgb.shape[1] == 3:
+            pcl_time_rgb.resize(new_size)
+        else:
+            # no color info
+            pcl_time_rgb = None
+    
+    return pcl_time, pcl_time_rgb
+
+
 def standardize_bbox2(pcl1, pcl2, points_per_object):
     """
     Standardizes to [-0.5, 0.5], but considering the union of pcl1 and pcl2, so the standardization is not independent
@@ -57,19 +78,28 @@ def standardize_bbox2(pcl1, pcl2, points_per_object):
 
 
 def main2(partial_file_path, evaluated_file_path, out_file_path, num_points_per_object):
-    filename, file_extension = os.path.splitext(out_file_path)
+    filename, out_file_extension = os.path.splitext(out_file_path)
+    _, in_file_extension = os.path.splitext(partial_file_path)
     # filename = os.path.basename(filename)
     # folder = os.path.dirname(partial_file_path)
-
-    partial_pcl_time, color_partial_pcl_time = read_ply(partial_file_path)
-    evaluated_pcl_time, color_evaluated_pcl_time = read_ply(evaluated_file_path)
+    
+    if in_file_extension == '.ply':
+        partial_pcl_time, _ = read_ply(partial_file_path)
+        evaluated_pcl_time, color_evaluated_pcl_time = read_ply(evaluated_file_path)
+    elif in_file_extension == '.obj':
+        partial_pcl_time, _ = read_obj(partial_file_path)
+        evaluated_pcl_time, color_evaluated_pcl_time = read_obj(evaluated_file_path)
+    else:
+        print(f"unsupported {in_file_extension} file")
+        return
 
     for pcli in range(0, np.shape(partial_pcl_time)[0]):
         partial_pcl = partial_pcl_time[pcli, :, :]
         evaluated_pcl = evaluated_pcl_time[pcli, :, :]
-        # filter black and dark-gray points in evaluated_pcl
-        indices = np.where(color_evaluated_pcl_time[pcli][:, 0] >= 10)[0]
-        evaluated_pcl = evaluated_pcl[indices]
+        if color_evaluated_pcl_time is not None:
+            # filter black and dark-gray points in evaluated_pcl
+            indices = np.where(color_evaluated_pcl_time[pcli][:, 0] >= 10)[0]
+            evaluated_pcl = evaluated_pcl[indices]
 
         # standardize both pcl
         partial_pcl, evaluated_pcl = standardize_bbox2(partial_pcl, evaluated_pcl, num_points_per_object)
@@ -112,7 +142,7 @@ def main2(partial_file_path, evaluated_file_path, out_file_path, num_points_per_
             f.write(xml_content)
         f.close()
 
-        png_file = f"{filename}_{pcli:02d}" + file_extension  # os.path.join(folder, f"{filename}_restored_{pcli:02d}.png")
+        png_file = f"{filename}_{pcli:02d}" + out_file_extension  # os.path.join(folder, f"{filename}_restored_{pcli:02d}.png")
         print(['Running Mitsuba, loading: ', xmlFile])
         scene = mitsuba.load_file(xmlFile)
         render = mitsuba.render(scene)
@@ -126,6 +156,7 @@ def parse_args():
                         help="basename of the .ply partial and evaluated files, or name of the folder to process")
     parser.add_argument("-n", "--num_points_per_object", type=int, default=2048)
     parser.add_argument("-v", "--mitsuba_variant", type=str, choices=mitsuba.variants(), default="scalar_rgb")
+    parser.add_argument('-u', '--up_axis', type=str, choices=['x','y','z'], default='z', help='Axis considered height')
 
     parser.add_argument("--partial_suffix", type=str, default="_points.partial.ply")
     parser.add_argument("--evaluated_suffix", type=str, default="_evaluated_pc.ply")
@@ -157,12 +188,23 @@ def get_restored_renders(pattern):
 if __name__ == "__main__":
     args = parse_args()
     mitsuba.set_variant(args.mitsuba_variant)
+    
+    up = args.up_axis
+    up_x, up_y, up_z = ('x' in up), ('y' in up), ('z' in up)
+    up_vec = f'{1&up_x},{1&up_y},{1&up_z}'
+    xml_head = xml_head.format(up_vec)
+    xml_tail = xml_tail.format(up_vec)
+    
     pair_paths = get_pairs_in_folder(args.partial_suffix, args.evaluated_suffix, args.basename)
     for p in pair_paths:
-        main2(p[0],
-              p[1],
-              p[0].rsplit(args.partial_suffix, 1)[0] + args.restored_suffix,
-              args.num_points_per_object)
+        print(p[0], p[1], p[0].rsplit(args.partial_suffix, 1)[0] + args.restored_suffix)
+        try:
+            main2(p[0],
+                  p[1],
+                p[0].rsplit(args.partial_suffix, 1)[0] + args.restored_suffix,
+                args.num_points_per_object)
+        except Exception as e:
+            print(e)
 
     if len(pair_paths) > 1:
         merge_renders(get_restored_renders(args.basename+args.restored_suffix), args.basename[:-1] + "combined.png")
